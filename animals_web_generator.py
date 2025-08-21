8219eecb4ee45dd878e6e66fbcb473e0ffeda470
Eric Draegerdt <ericdragerdt@macbookairvoneric.fritz.box>
2025-08-21 14:49:01 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -20,14 +20,23 @@ html_data = load_html('animals_template.html')
 animals_data_string = ""
 
 for animal in animals_data:
-    animals_data_string += f"Name: {animal['name']}\n"
-    animals_data_string += f"Diet: {animal['characteristics']['diet']}\n"
+    animals_data_string += '<li class="cards__item">\n'
+    animals_data_string += f"Name: {animal['name']}</br>\n"
+    animals_data_string += f"Diet: {animal['characteristics']['diet']}</br>\n"
     locations = "Locations: "
-    for location in animal['locations']:
-        locations += f"{location}, "
-    animals_data_string += locations + "\n"
+    if len(animal['locations']) > 1:
+        for i, location in enumerate(animal['locations']):
+            if i == len(animal['locations']) - 1:
+                locations += f"{location}"
+            else:
+                locations += f"{location}, "
+        animals_data_string += locations + "</br>\n"
+    else:
+        animals_data_string += animal['locations'][0] + "</br>\n"
     if "type" in animal['taxonomy']:
-        animals_data_string += f"{animal['taxonomy']['type']}\n"
+        animals_data_string += f"{animal['taxonomy']['type']}</br>\n"
+    animals_data_string += '</li>\n'
+
 
 html_data = html_data.replace("__REPLACE_ANIMALS_INFO__", animals_data_string)
 
