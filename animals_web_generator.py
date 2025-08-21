7eea5fe43d870d744c46157be36d316f5c8e3590
Eric Draegerdt <ericdragerdt@macbookairvoneric.fritz.box>
2025-08-21 15:27:34 +0200

    formatted code that animal serialization is in its own function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,17 +13,13 @@ def load_html(file_path):
       return html_content
 
 
-animals_data = load_data('animals_data.json')
-
-html_data = load_html('animals_template.html')
-
-animals_data_string = ""
-
-for animal in animals_data:
-    animals_data_string += '<li class="cards__item">\n'
-    animals_data_string += f'  <div class="card__title">{animal['name']}</div>\n'
-    animals_data_string += '  <p class="card__text">\n'
-    animals_data_string += f'    <strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n'
+def serialize_animal(animal):
+    """ Serializes an animal """
+    animal_obj = ''
+    animal_obj += '<li class="cards__item">\n'
+    animal_obj += f'  <div class="card__title">{animal['name']}</div>\n'
+    animal_obj += '  <p class="card__text">\n'
+    animal_obj += f'    <strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n'
     locations = "    <strong>Locations:</strong> "
     if len(animal['locations']) > 1:
         for i, location in enumerate(animal['locations']):
@@ -33,11 +29,22 @@ for animal in animals_data:
                 locations += f"{location}, "
     else:
         locations += animal['locations'][0]
-    animals_data_string += locations + "<br/>\n"
+    animal_obj += locations + "<br/>\n"
     if "type" in animal['characteristics']:
-        animals_data_string += f'    <strong> Type:</strong> {animal['characteristics']['type']}<br/>\n'
-    animals_data_string += "  </p>\n"
-    animals_data_string += '</li>\n'
+        animal_obj += f'    <strong> Type:</strong> {animal['characteristics']['type']}<br/>\n'
+    animal_obj += "  </p>\n"
+    animal_obj += '</li>\n'
+    return animal_obj
+
+animals_data = load_data('animals_data.json')
+
+html_data = load_html('animals_template.html')
+
+animals_data_string = ""
+
+for animal in animals_data:
+    animals_data_string += serialize_animal(animal)
+
 
 
 html_data = html_data.replace("__REPLACE_ANIMALS_INFO__", animals_data_string)
